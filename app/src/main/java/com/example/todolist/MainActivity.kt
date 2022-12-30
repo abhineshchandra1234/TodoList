3fb7320fa629505d100b6774e1f98a4697598ae8
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2022-12-30 15:58:50 +0530

    layouts & room entity

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//https://youtu.be/SVJFBAKcZSQ
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
